9127ecdf422ff360a0e9f67cff513886fb3c7d3c
Rafael A S Coelho <rafael.asilvacoelho@gmail.com>
2019-09-18 15:04:41 -0300

    Add some functional programming
    - Creating some functional programming style in Java in
    order to explorer Java 8 capabilities. For a while, have
    been using streams, comparators and collectors utilities.

---
+++ functional/streams/src/main/java/com/rscoelho/java/functional/FunctionalExamples.java
@@ -0,0 +1,60 @@
+package com.rscoelho.java.functional;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class FunctionalExamples {
+    public static void main(String[] args) {
+        Comparator<Person> asc = Person::ageDifference;
+        Comparator<Person> desc = asc.reversed();
+
+        List<Person> people = Arrays.asList(new Person("Ana", 30), new Person("John", 32), new Person("Bob", 18),
+                new Person("Lisa", 18));
+
+        System.out.println("Ascending is: ");
+        people.stream().sorted(asc).collect(toList()).forEach(System.out::println);
+
+        System.out.println("\nDescending is: ");
+        people.stream().sorted(desc).collect(toList()).forEach(System.out::println);
+
+        System.out.println("\nSorting by name: ");
+        people.stream().sorted((p1, p2) -> p1.getName().compareTo(p2.getName())).collect(toList()).forEach(System.out::println);
+        
+        System.out.println("\nGetting Youngest: ");
+        people.stream().min(asc).ifPresent(System.out::println);
+
+        System.out.println("\nGetting Oldest: ");
+        people.stream().max(asc).ifPresent(System.out::println);
+    }
+    
+    
+
+    public static class Person {
+        private final String name;
+        private final int age;
+
+        public Person(final String name, final int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public int ageDifference(final Person other) {
+            return age - other.age;
+        }
+
+        public String toString() {
+            return String.format("%s - %d", name, age);
+        }
+    }
+}
